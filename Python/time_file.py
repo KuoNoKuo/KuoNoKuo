d69796fb744c38e7ad155b41cf723ffddbb93d0b
Alessio Liu <liuyijiealessio435@gmail.com>
2022-09-08 12:02:02 +0200

    Create time_file.py

---
+++ Python/time_file.py
@@ -0,0 +1,7 @@
+import time 
+#Used to set delays and clock
+print(0)
+time.sleep(1) # '.sleep' = wait | parameters = seconds to wait
+print(1)
+
+print(time.localtime()) # '.localtime' is used to get the current time | The output =list
